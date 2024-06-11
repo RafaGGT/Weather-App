93e01880142b0529fe4ee0f10163af0993537940
Rafael <rg.gallegostapia@gmail.com>
2024-06-10 22:59:51 -0400

    commit 5to

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = process.env.APIKEY;
+const apiKey = //"aqui va la secuencia del codigo que se te otorga"//;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 const searchBox = document.querySelector(".search input");
